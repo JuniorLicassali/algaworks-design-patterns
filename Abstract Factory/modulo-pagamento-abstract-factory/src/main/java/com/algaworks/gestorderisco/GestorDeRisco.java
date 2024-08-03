8d26f3fb13573477bf61ca2aad73f3500c83f8fe
JuniorLicassali <licassali88@gmail.com>
2024-08-03 16:23:44 -0300

    Abstract Factory

---
+++ Abstract Factory/modulo-pagamento-abstract-factory/src/main/java/com/algaworks/gestorderisco/GestorDeRisco.java
@@ -0,0 +1,7 @@
+package com.algaworks.gestorderisco;
+
+import java.math.BigDecimal;
+
+public interface GestorDeRisco {
+	public void avaliarRisco(String cartao, BigDecimal valor) throws AlertaDeRiscoException;
+}
